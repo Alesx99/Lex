348d8e26569e5495ace10bde5ea142385fcfc4b6
Alessio <alesx1999@gmail.com>
2026-05-26 15:02:02 +0200

    fix: increment PWA cache to v2 and cache Roman Art assets

--- sw.js
+++ sw.js
@@ -1,17 +1,33 @@
-const CACHE_NAME = 'lex-cache-v1';
+const CACHE_NAME = 'lex-cache-v2';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './js/search_db.js',
   './js/quiz_db.js',
-  './js/glossary_db.js'
+  './js/glossary_db.js',
+  './arte_romana/index.html',
+  './arte_romana/style.css',
+  './arte_romana/quiz.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => self.clients.claim())
   );
 });
 
