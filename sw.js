91604de62a7b30960c18f159681515cbe3f03222
Alessio <alesx1999@gmail.com>
2026-07-06 10:52:10 +0200

    Download Chart.js locally, remove sourceMappingURL comment to resolve browser 404 warnings, and add to PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lex-cache-v19';
+const CACHE_NAME = 'lex-cache-v20';
 const ASSETS = [
   './',
   './index.html',
@@ -11,6 +11,7 @@ const ASSETS = [
   './js/search_db.js',
   './js/quiz_db.js',
   './js/glossary_db.js',
+  './js/chart.umd.min.js',
   './arte_romana/index.html',
   './arte_romana/style.css',
   './arte_romana/quiz.html',
